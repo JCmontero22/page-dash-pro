d29083cd325eae5c99f5b6a00f6b05da503f2d2f
Johan Montero <johan00712@gmail.com>
2025-12-16 13:42:03 -0500

    Arreglo de cartegorias

--- ajax/consultaCategorias.php
+++ ajax/consultaCategorias.php
@@ -3,7 +3,7 @@
     require_once('../config/conexion.php');
     $db = new conexion();
 
-    $query = "SELECT * FROM categorias WHERE estado_categoria = 1";
+    $query = "SELECT * FROM categorias_dash WHERE estado_categoria = 1";
 
     $categorias = $db->select($query);
 
