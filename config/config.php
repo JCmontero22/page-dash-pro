33d3d2d746f90050a04ff3c1be91902a72993620
Johan Montero <johan00712@gmail.com>
2025-12-16 13:34:21 -0500

    Actualizacion conexion

--- config/config.php
+++ config/config.php
@@ -7,6 +7,6 @@
     DEFINE('DB_NAME', 'dash_pro'); */
     
     define('DB_HOST', '127.0.0.1');
-    define('DB_NAME', 'jcmonter_productos_aseo');
+    define('DB_NAME', 'jcmonter_dev');
     define('DB_USER', 'jcmonter_dev94');    
     define('DB_PASS', 'Stratovarius94@');
